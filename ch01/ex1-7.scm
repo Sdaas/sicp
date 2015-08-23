dc2eee687ce6cbe8399b21090b9f98c7d94c2169
Soumendra Daas <daas.soumendra@gmail.com>
2015-08-23 09:37:38 +0530

    refactored to use block structures - i.e, nesting of definitions

--- ch01/ex1-7.scm
+++ ch01/ex1-7.scm
@@ -1,18 +1,21 @@
+(define (mysqrt x) 
 
-(define (average x y) ( / (+ x y) 2))
-(define (error x y) (- x (* y y )))
+    (define (average x y) ( / (+ x y) 2))
+    (define (error x y) (- x (* y y )))
 
-; Formula to compute the next estimate of y
-(define (nexty x y) (average y (/ x y)))
+    ; Formula to compute the next estimate of y
+    (define (nexty x y) (average y (/ x y)))
 
-; We have a good esimate if the current estimate is within 0.1%
-; of the previous estimate
-(define (isgood yprev y) ( < (/ (abs (- y yprev )) yprev) 0.001 ))
+    ; We have a good esimate if the current estimate is within 0.1%
+    ; of the previous estimate
+    (define (isgood yprev y) ( < (/ (abs (- y yprev )) yprev) 0.001 ))
 
-(define (sqrt-iteration x yprev y)
-    (if (isgood yprev y)  y (sqrt-iteration x y (nexty x y)))
+    (define (sqrt-iteration x yprev y)
+        (if (isgood yprev y)  y (sqrt-iteration x y (nexty x y)))
+    )
+    
+    ; Evaluate the square root
+    (sqrt-iteration x 1 0.1)
 )
-
-(define (mysqrt x) (sqrt-iteration x 1 0.1))
 
 ; Need to be careful here about divide by zeros ...
