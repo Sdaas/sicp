a7efd3bc3e649749f1b9a210699eb62cf0876afe
Soumendra Daas <daas.soumendra@gmail.com>
2015-08-30 09:22:33 +0530

    added unit tests

--- ch01/ex1-11.scm
+++ ch01/ex1-11.scm
@@ -34,4 +34,24 @@
 ; f(4)  = f(3) + 2f(2) + 3f(1) = 4 + 2.2 + 3.1 = 4 + 4 + 3 = 11
 ; f(5)  = f(4) + 2f(3) + 3f(2) = 11 + 2.4 + 3.2 = 11 + 8 + 6 = 25
 ; f(6)  = f(5) + 2f(4) + 3f(3) = 25 + 2.11 + 3.4 = 25 + 22 + 12 = 59
+; f(7)  = f(6) + 2f(5) + 3f(4) = 59 + 2.25 + 3.11 = 59 + 50 + 33 = 142
 
+; All the unit tests
+(load "../test-manager/load.scm")
+
+(in-test-group ex1-11tests
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (check (= 0 (f 0)) "f(0) should be 0" )
+        (check (= 1 (f 1)) "f(1) should be 1" )
+        (check (= 2 (f 2)) "f(2) should be 2" )
+        (check (= 4 (f 3)) "f(3) should be 4" )
+        (check (= 11 (f 4)) "f(4) should be 11" )
+        (check (= 25 (f 5)) "f(5) should be 25" )
+        (check (= 59 (f 6)) "f(6) should be 59" )
+        (check (= 142 (f 7)) "f(7) should be 142" )
+    )
+)
+
+; run all the tests
+(run-registered-tests)
--- ch01/ex1-2.scm
+++ ch01/ex1-2.scm
@@ -6,3 +6,16 @@
 (define num (+ 4 5 (- 2 (- 3 (+ 6 (/ 4 5))))))
 (define den (* 3 (- 6 2) (- 2 7) ))
 (/ num den)
+
+; Define all the unit tests
+(load "../test-manager/load.scm")
+
+(in-test-group ex1-2tests
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (check (= -37/150 (/ num den)) "should be -37/150" )
+    )
+)
+
+; run all the tests
+(run-registered-tests)
