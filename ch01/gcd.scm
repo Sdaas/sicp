e6674ed45f56fa4900e21bba0f080d4f7fcb03b3
Soumendra Daas <daas.soumendra@gmail.com>
2015-08-30 16:08:40 +0530

    implemented GCD as per Section 1.2.5

---
+++ ch01/gcd.scm
@@ -0,0 +1,24 @@
+; GCD (a,b) = GCD(b,r) where r = a mod b
+(define (gcd a b)
+    (if (= 0 b)
+        a
+        (gcd b (remainder a b))
+    )
+)
+
+
+; Define all the unit tests
+(load "../test-manager/load.scm")
+
+(in-test-group gcd-tests
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (check (= 3 (gcd 6 9)) "GCD of 6 and 9 is 3" )
+        (check (= 12 (gcd 660 252)) "GCD of 660 and 252 is 12")
+        (check (= 1 (gcd 11 17)) "GCD of 11 and 17 is 1")
+    )
+)
+
+
+; run all the tests
+(run-registered-tests)
