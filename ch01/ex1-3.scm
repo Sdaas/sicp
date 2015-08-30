fc1a8d5d8c298092a1229d945bf17ff90268a9af
Soumendra Daas <daas.soumendra@gmail.com>
2015-08-30 08:53:44 +0530

    Added unit tests

--- ch01/ex1-3.scm
+++ ch01/ex1-3.scm
@@ -15,3 +15,24 @@
 ; 1 1 1
 ; 1 2 2
 ; 1 1 2
+
+; All the unit tests
+(load "../test-manager/load.scm")
+
+(in-test-group ex1-3tests
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (check (= 13 (myfunc 3 1 2)) "should return 13" )
+        (check (= 13 (myfunc 1 2 3)) "should return 13" )
+        (check (= 2 (myfunc 1 1 1)) "should return 2" )
+        (check (= 8 (myfunc 1 2 2)) "should return 8" )
+        (check (= 5 (myfunc 1 1 2)) "should return 5" )
+    )
+)
+
+; run all the tests
+(run-registered-tests)
+
+; Can run individual groups or tests with
+;(run-test '(ex1-3tests))
+;(run-test '(simple-stuff arithmetic))
