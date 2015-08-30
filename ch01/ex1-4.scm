6faa1a7ae911f1c1122e0eb2536e8677c9d807fc
Soumendra Daas <daas.soumendra@gmail.com>
2015-08-30 10:15:46 +0530

    added unit tests

--- ch01/ex1-4.scm
+++ ch01/ex1-4.scm
@@ -1,3 +1,19 @@
 ; A procedure can evaluate to an operator
 ; In this case, if b > 0, it returns the "+" operator, else it returns the "-" opr
 (define (a-plus-abs-b a b) ((if (> b 0) + -) a b))
+
+
+; All the unit tests
+(load "../test-manager/load.scm")
+
+(in-test-group ex1-4tests
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (check (= 0 (a-plus-abs-b 0 0)) "0 + abs(0) should be 0" )
+        (check (= 5 (a-plus-abs-b 2 3)) "2 + abs(3) should be 5" )
+        (check (= 5 (a-plus-abs-b 2 -3)) "2 + abs(-3) should be 5" )
+    )
+)
+
+; run all the tests
+(run-registered-tests)
--- ch01/ex1-7.scm
+++ ch01/ex1-7.scm
@@ -7,14 +7,40 @@
 
     ; We have a good esimate if the current estimate is within 0.1%
     ; of the previous estimate
-    (define (isgood yprev y) ( < (/ (abs (- y yprev )) yprev) 0.001 ))
+    (define (isgood yprev y) ( < (/ (abs (- y yprev )) yprev) 0.0001 ))
 
     (define (sqrt-iteration yprev y)
         (if (isgood yprev y)  y (sqrt-iteration y (nexty y)))
     )
     
+    ; Need to be careful here about divide by zeros ...
     ; Evaluate the square root
-    (sqrt-iteration 1 0.1)
+    (if (= x 0.0) 
+        0
+        (sqrt-iteration 1 0.1)
+    )
 )
 
-; Need to be careful here about divide by zeros ...
+
+; All the unit tests
+(load "../test-manager/load.scm")
+
+
+; tests for the mysqrt2 function
+(in-test-group msqrt2-tests
+    (define tol 0.0001)  ; error tolerance
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (assert-in-delta 0.0000 (mysqrt2 0) tol "sqrt 0" )
+        ;(assert-in-delta 0.0000 (mysqrt2 0.000001 ) tol "sqrt 0.0" )
+        (assert-in-delta 1.0000 (mysqrt2 1.0 ) tol "sqrt 1" )
+        (assert-in-delta 1.41421 (mysqrt2 2.0 ) tol "sqrt 2" )
+        (assert-in-delta 10.00000 (mysqrt2 100.0 ) tol "sqrt 10" )
+        (assert-in-delta 0.31622 (mysqrt2 0.1 ) tol "sqrt .1" )
+        (assert-in-delta 0.1000 (mysqrt2 0.01 ) tol "sqrt .01" )
+        (assert-in-delta 0.03162 (mysqrt2 0.001 ) tol "sqrt 0.001" )
+    )
+)
+
+; run all the tests
+(run-registered-tests)
