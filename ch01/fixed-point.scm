d2c17418c3ab75900e26a448a66ae9c6396f649c
Soumendra Daas <daas.soumendra@gmail.com>
2015-09-02 19:35:40 +0530

    refactored fixed-point tests into new file

--- ch01/fixed-point.scm
+++ ch01/fixed-point.scm
@@ -17,32 +17,5 @@
     )
 )
 
-; y = sqrt(x) => y^2 = x  =>  y = x / y 
-(define (mysqrt x)
-    (fixed-point  (lambda (y) (/ x y)) 1.0 )
-)
 
-; All the unit tests
-(load "../test-manager/load.scm")
-
-(in-test-group fixed-point-tests
-    (define tol 0.00001)  ; error tolerance
-    ; Each of these will become a separate anonymous one-form test
-    (define-each-test
-        (assert-in-delta 0.73908 (fixed-point cos 1.0) tol "cos(x)" )
-        (assert-in-delta 1.25873 (fixed-point (lambda (x) (+ (cos x) (sin x))) 1.0 ) tol "cos(x) + sin(x)" )
-    )
-)
-
-(in-test-group sqrt-tests
-    (define tol 0.00001)  ; error tolerance
-    ; Each of these will become a separate anonymous one-form test
-    (define-each-test
-        (assert-in-delta 1.41421 (mysqrt 2.0) tol "sqrt(2)" )
-        (assert-in-delta 8.66025 (mysqrt 75.0) tol "sqrt(75)" )
-    )
-)
-
-; run all the tests
-(run-registered-tests)
 
