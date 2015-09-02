e97211cc8ab3e04e4d0562d179887e9f68637fa6
Soumendra Daas <daas.soumendra@gmail.com>
2015-09-02 19:31:13 +0530

    refactored next-guess to be the average .."

--- ch01/fixed-point.scm
+++ ch01/fixed-point.scm
@@ -2,16 +2,24 @@
 ; f(x) = x
 
 (define (fixed-point f guess)
-    (define next (f guess)) ; This will be the next guess if needed
+    (define (average x y) ( / (+ x y) 2))
+    (define fg (f guess)) ;
+    (define next-guess (average fg guess)) ; 
+
     (display guess)
     (display " f=")
-    (display next)
+    (display fg)
     (newline)
 
     (cond
-        (( < (abs (- guess next)) 0.00001 ) guess )
-        (else (fixed-point f next))
+        (( < (abs (- guess fg)) 0.00001 ) guess )
+        (else (fixed-point f next-guess))
     )
+)
+
+; y = sqrt(x) => y^2 = x  =>  y = x / y 
+(define (mysqrt x)
+    (fixed-point  (lambda (y) (/ x y)) 1.0 )
 )
 
 ; All the unit tests
@@ -26,7 +34,15 @@
     )
 )
 
+(in-test-group sqrt-tests
+    (define tol 0.00001)  ; error tolerance
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (assert-in-delta 1.41421 (mysqrt 2.0) tol "sqrt(2)" )
+        (assert-in-delta 8.66025 (mysqrt 75.0) tol "sqrt(75)" )
+    )
+)
+
 ; run all the tests
 (run-registered-tests)
-
 
