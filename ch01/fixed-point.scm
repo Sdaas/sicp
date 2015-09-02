5b31d831ffad23a2f97ba8315751cddf05e6af12
Soumendra Daas <daas.soumendra@gmail.com>
2015-09-02 19:19:06 +0530

    refactored so f() is not computed unnessarily

--- ch01/fixed-point.scm
+++ ch01/fixed-point.scm
@@ -2,13 +2,31 @@
 ; f(x) = x
 
 (define (fixed-point f guess)
+    (define next (f guess)) ; This will be the next guess if needed
     (display guess)
     (display " f=")
-    (display (f guess))
+    (display next)
     (newline)
+
     (cond
-        (( < (abs (- guess (f guess))) 0.00001 ) guess )
-        (else (fixed-point f (f guess)))
+        (( < (abs (- guess next)) 0.00001 ) guess )
+        (else (fixed-point f next))
     )
 )
+
+; All the unit tests
+(load "../test-manager/load.scm")
+
+(in-test-group fixed-point-tests
+    (define tol 0.00001)  ; error tolerance
+    ; Each of these will become a separate anonymous one-form test
+    (define-each-test
+        (assert-in-delta 0.73908 (fixed-point cos 1.0) tol "cos(x)" )
+        (assert-in-delta 1.25873 (fixed-point (lambda (x) (+ (cos x) (sin x))) 1.0 ) tol "cos(x) + sin(x)" )
+    )
+)
+
+; run all the tests
+(run-registered-tests)
+
 
