21badb97ad5806a1e01e69d035c20ffc69e41faf
Soumendra Daas <daas.soumendra@gmail.com>
2015-09-14 19:59:20 +0530

    make-rat ensures that num and den are mutually prime
    When creating a rational number, make-rat ensures that the numerator
    and denominator are always mutually prime. Also, if the numerator is 0,
    the the denominator is just set to 1.

--- ch02/ex2-01-tests.scm
+++ ch02/ex2-01-tests.scm
@@ -3,7 +3,6 @@
 ; Load the unit testing framework
 (load "../test-manager/load.scm")
 
-;define the unit tests
 (define-test (test1)
     ; Create a rational number with positive numerator and denominator
     (define rat (make-rat 4 5))
@@ -11,7 +10,7 @@
     (check (= (denom rat) 5) "Denominator should be 5")
 )
 
-;define the unit tests
+
 (define-test (test2)
     ; Create a rational number with negative numerator
     (define rat (make-rat -4 5))
@@ -19,7 +18,7 @@
     (check (= (denom rat) 5) "Denominator should be 5")
 )
 
-;define the unit tests
+
 (define-test (test3)
     ; Create a rational number with negative denominator
     (define rat (make-rat 4 -5))
@@ -27,7 +26,6 @@
     (check (= (denom rat) 5) "Denominator should be 5")
 )
 
-;define the unit tests
 (define-test (test4)
     ; Create a rational number with negative numerator and denominator
     (define rat (make-rat -4 -5))
@@ -35,20 +33,41 @@
     (check (= (denom rat) 5) "Denominator should be 5")
 )
 
-;define the unit tests
+
 (define-test (test5)
     ; Create a rational number with zero numerator
     (define rat (make-rat 0 -7))
     (check (= (numer rat) 0) "Numerator should be 0")
-    (check (= (denom rat) 7) "Denominator should be 7")
+    (check (= (denom rat) 1) "Denominator should be 1")
 )
 
-;define the unit tests
+
 (define-test (test6)
     ; Create a rational number with zero numerator
     (define rat (make-rat 0 8))
     (check (= (numer rat) 0) "Numerator should be 0")
-    (check (= (denom rat) 8) "Denominator should be 8")
+    (check (= (denom rat) ) "Denominator should be 1")
+)
+
+(define-test (test7)
+    ; numerator and denominator are mutually prime
+    (define rat (make-rat 13 17))
+    (check (= (numer rat) 13) "Numerator should be 13")
+    (check (= (denom rat) 17) "Denominator should be 17")
+)
+
+(define-test (test8)
+    ; numerator and denominator are NOT mutually prime
+    (define rat (make-rat 10 20))
+    (check (= (numer rat) 1) "Numerator should be 1")
+    (check (= (denom rat) 2) "Denominator should be 2")
+)
+
+(define-test (test9)
+    ; numerator and denominator are NOT mutually prime
+    (define rat (make-rat 15 -20))
+    (check (= (numer rat) -3) "Numerator should be -3")
+    (check (= (denom rat) 4) "Denominator should be 4")
 )
 
 ; run all the tests
--- ch02/ex2-01.scm
+++ ch02/ex2-01.scm
@@ -1,13 +1,22 @@
+(load "../ch01/gcd.scm")
 
 ; make a rational number
 (define (make-rat num den)
 
     ; TODO handling of 0 in num and/or denom
-    (cond
-        ((and (>= num 0) (> den 0)) (cons num den))
-        ((and (< num 0) (> den 0)) (cons  num den))
-        ( else (cons (- num) (- den)))
+    (define g (gcd num den))
+    (if (= 1 g)
+        ; num and denom are mutually prime, so just handle the signs
+        (cond
+            ((and (>= num 0) (> den 0)) (cons num den))
+            ((and (< num 0) (> den 0)) (cons  num den))
+            ( else (cons (- num) (- den)))
+        )
+        ; make a rational number after divding by the hcf
+        (make-rat (/ num g) (/ den g))
     )
+    
+    
 )
 
 (define (numer rat) (car rat))
