10e1e8f4159e2aaa2c7ced19023ba97b7f52dd90
Soumendra Daas <daas.soumendra@gmail.com>
2015-09-17 12:27:39 +0530

    Implemented 'add' for Ex2-6

--- ch02/ex2-06.scm
+++ ch02/ex2-06.scm
@@ -15,3 +15,31 @@
 ; (lambda (f) (lambda (x) (f x)))
 
 (define one (lambda (f) (lambda (x) (f x))))
+
+; What is two ? 
+; Consider (n f) where n = "one"  (lambda (f) (lambda (x) (f x)))
+; This evaluates to (lambda (x) (f x))
+; Substittig this back in ((n f) x), this evalutes to (f x)
+; So add-1 for n="one" evaluates to
+;( lambda (f) ( lambda (x) (f (f x))))
+
+(define two ( lambda (f) ( lambda (x) (f (f x)))))
+
+;Doing add-1 for n="two" evaluates to 
+(define three ( lambda (f) ( lambda (x) (f (f (f x))))))
+
+;Note that for any number n > 0, it is basically the number of time (f x) has been wrapped with f
+
+; Lets look at what ((n f) x) does. 
+; for n="one" this is  ( [ lambda (x) (f x)] x )  evaluates to (f x)
+; for n="two" this is  ( [ lambda (x) (f (f x)) ] x ) evalutes to (f (f x))
+;
+; In general, for any church numeral m, ((m f) y) wraps y with f another m times
+; And for any church numeral n ((n f) x) wraps x with f another m times
+; Substituting y for x ..
+; ( (m f) ((n f) x)  seems to wrap f m+n times around x => which means it it the "m+n" number
+
+(define (add m n)
+    (lambda (f) (lambda (x) ((m f) ((n f) x))))
+)
+
