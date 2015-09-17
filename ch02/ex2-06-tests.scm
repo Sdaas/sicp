5222a4a02e1a7b06aeb2ad54e74c48cf396e9a44
Soumendra Daas <daas.soumendra@gmail.com>
2015-09-17 13:33:49 +0530

    print out the church numerals

---
+++ ch02/ex2-06-tests.scm
@@ -0,0 +1,20 @@
+(load "ex2-06.scm")
+
+; The intuition is that for the n-th church numeral, 
+; f will be called n times
+; (define church-n ( lambda (f) ( lambda (x) (f ( ... (f x))))))
+
+;(define two ( lambda (f) ( lambda (x) (f (f x)))))
+;
+;(two foo) => (lambda (x) ( foo ( foo x)))
+
+(define (print x) (display "X"))
+
+((zero print) "dummy") ; should not print X
+(newline)
+((one print) "dummy") ; should print X once
+(newline)
+((two print) "dummy") ; should print X twice
+(newline)
+((three print) "dummy") ; should print X 3 times
+(newline)
